124ae606a5ed0ef09c845dfae56c922e77af27ae
CollinEdward <manorengaard@icloud.com>
2021-11-22 17:00:05 +0100

    added search feature.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import google
+
+try:
+    from googlesearch import search
+except ImportError:
+    print("import error google search")
 
 def main():
-    pass
+    q = input("What do you want to search up?: ")
 
+    for x in search(q, num=20, pause= 2):
+        print(x)
 
 
 
