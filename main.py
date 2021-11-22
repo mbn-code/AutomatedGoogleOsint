ced2ae2c8623fbb9f4d3acc108085ec013b0debb
CollinEdward <manorengaard@icloud.com>
2021-11-22 17:52:35 +0100

    Comments added for explanation + write to history file.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-
-try:
+try: # checks if googlesearch is installed in pip
     from googlesearch import search
 except ImportError:
-    print("import error google search")
+    print("import error 'googlesearch'")
+    print("Run: pip3 install googlesearch") 
 
 def main():
+    # search string
     q = input("What do you want to search up?: ")
 
+    # For the search history file, opens and writes the results
     with open("google_search_history.txt", "w") as google_results:
         for x in search(q, num=20, pause= 2):
             print(x)
             google_results.write(x + "\n")
-            
+
 
 if __name__ == '__main__':
     main()
