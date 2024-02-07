2c5f57e7a9da6f6af1960531b31abfb9fcaff420
mbn <66748817+CollinEdward@users.noreply.github.com>
2024-02-07 21:44:28 +0100

    Update googlesearch package name in error message

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-try: # checks if googlesearch is installed in pip
+try: # checks if googlesearch-python is installed in pip
     from googlesearch import search
 except ImportError:
-    print("import error 'googlesearch'")
-    print("Run: pip3 install googlesearch") 
+    print("import error 'googlesearch-python'")
+    print("Run: pip3 install googlesearch-python")
+    exit()  # Stop the program if googlesearch-python is not installed
 
 def main():
     # search string
@@ -10,10 +11,9 @@ def main():
 
     # For the search history file, opens and writes the results
     with open("google_search_history.txt", "w") as google_results:
-        for x in search(q, num=20, pause= 2):
+        for x in search(q, num_results=20):
             print(x)
             google_results.write(x + "\n")
-
 
 if __name__ == '__main__':
     main()
